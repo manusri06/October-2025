8787a332a10f36bcf7d1002a6ba3a932618442da
V. Manusri <velpula.manusri06@gmail.com>
2025-10-07 14:32:37 +0530

    Time: 0 ms (100%), Space: 18.2 MB (20.53%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,15 +2,17 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         dict = {}
         stack=[]
+        nums2 = nums2[::-1]
         for i in nums2:
-            while stack and stack[-1] < i:
-                e = stack.pop()
-                dict[e] = i
-            stack.append(i)
-        
-        for i in range(len(nums1)):
-            if nums1[i] in dict:
-                nums1[i] = dict[nums1[i]]
+            while stack and stack[-1] <= i:
+                stack.pop()
+            if not stack:
+                dict[i] = -1
             else:
-                nums1[i] = -1
-        return nums1
+                dict[i] = stack[-1]
+            stack.append(i)
+
+        res = []
+        for i in nums1:
+            res.append(dict[i])
+        return res
